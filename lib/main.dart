edb7d7e01b85b948f0b1e3333d304dc68640740a
pchchv <ipchchv@gmail.com>
2025-10-07 21:04:26 +0300

    feat: create a BigCard widget

--- lib/main.dart
+++ lib/main.dart
@@ -55,3 +55,14 @@ class MyHomePage extends StatelessWidget {
     );
   }
 }
+
+class BigCard extends StatelessWidget {
+  const BigCard({super.key, required this.pair});
+
+  final WordPair pair;
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(pair.asLowerCase);
+  }
+}
